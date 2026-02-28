c71904c3c6e474a61658a56a96745d2fbebc28ea
fastmoneymozambique <bloudbilo@gmail.com>
2026-02-28 20:24:31 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,50 @@
 plugins {
-    alias(libs.plugins.android).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.fastmoneymozambique.simpleflashlight"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.fastmoneymozambique.simpleflashlight"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.github.SimpleMobileTools:Simple-Commons:37078b151c")
 }
